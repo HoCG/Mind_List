65c29e4fe8ba9e37441668a217625c6da70e642a
HoCG <rhwkd5012@naver.com>
2022-03-06 18:58:29 +0900

    test

---
+++ components/TestComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h3>테스트 입니다.</h3>
+    <h3>{{number}}</h3>
+  </div>
+</template>
+<script>
+  export default {
+    data(){
+      return{
+        number: 0
+      }
+    },
+    mounted(){
+      this.number = 30;
+    }
+  }
+</script>
+<style>
+
+</style>
